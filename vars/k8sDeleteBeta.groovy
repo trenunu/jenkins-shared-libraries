a3387acfb0b6f7613dae9c7098be47980bd220ce
trenunu <trenunu@gmail.com>
2020-05-14 07:50:10 +1200

    Update k8sDeleteBeta.groovy

--- vars/k8sDeleteBeta.groovy
+++ vars/k8sDeleteBeta.groovy
@@ -1,6 +1,5 @@
 def call(project) {
     chartName = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}"
-    sh """helm delete ${chartName.toLowerCase()} \
-        --tiller-namespace ${project}-build \
-        --purge"""
+    sh """helm uninstall ${chartName.toLowerCase()} \
+        --namespace ${project}-build"""
 }
