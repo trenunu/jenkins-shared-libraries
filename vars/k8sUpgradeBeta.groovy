ad855aba56a5eeb6358a70669d54d889235b8b53
trenunu <trenunu@gmail.com>
2020-05-14 07:48:25 +1200

    Update k8sUpgradeBeta.groovy

--- vars/k8sUpgradeBeta.groovy
+++ vars/k8sUpgradeBeta.groovy
@@ -2,10 +2,9 @@ def call(project, domain, extraValues = "") {
     chartName = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}"
     tagBeta = "${currentBuild.displayName}-${env.BRANCH_NAME}"
     addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
-    sh """helm upgrade \
+    sh """helm upgrade -i \
         ${chartName.toLowerCase()} \
-        helm/${project} -i \
-        --tiller-namespace ${project}-build \
+        helm/${project} \
         --set image.tag=${tagBeta} \
         --set ingress.host=${addr.toLowerCase()} \
         ${extraValues}"""
