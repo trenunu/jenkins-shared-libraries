2dcb67b074af4a9c9b2ff4177efc5431f4d96285
trenunu <trenunu@gmail.com>
2020-05-15 06:10:33 +1200

    Update k8sFuncTestGolang.groovy

--- vars/k8sFuncTestGolang.groovy
+++ vars/k8sFuncTestGolang.groovy
@@ -1,6 +1,7 @@
 def call(project, domain) {
     addr = "${project}-${env.BUILD_NUMBER}-${env.BRANCH_NAME}.${domain}"
     sh "go get -d -v -t"
+    sh "go mod vendor"
     sh """ADDRESS=${addr} \
         go test ./... -v \
         --run FunctionalTest"""
